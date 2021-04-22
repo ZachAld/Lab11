b0c280fb6c01621f5682bac2a2f9133af5bbfbfb
Zach Alderson <zacharyalderson@gmail.com>
2021-04-22 17:21:37 -0500

    Added recursive function to calculate fibonacci numbers

--- fiboRecursive.m
+++ fiboRecursive.m
@@ -1,3 +1,11 @@
-function [] = fiboRecursive ()
-
+function [fibonacciNumber] = fiboRecursive (n)
+    global count;
+    if n ==1
+        fibonacciNumber = 1;
+    elseif n == 2
+        fibonacciNumber = 1;
+    else
+        count = count + 1;
+        fibonacciNumber = fiboRecursive(n - 2) + fiboReceursive(n - 1);
+    end
 end
